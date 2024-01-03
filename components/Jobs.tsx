aec30f5d040f6ac30be3fbcd77a0ff2cfddd6779
Jonathan <jonlee121198@gmail.com>
2024-01-03 21:26:27 +0800

    shifted individual components to main page

---
+++ components/Jobs.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import { getJob } from "@/sanity/sanity.query";
+import type { JobType } from "@/types";
+import { PortableText } from "@portabletext/react";
+
+export default async function Jobs() {
+    const job: JobType[] = await getJob();
+    console.log(job)
+    return (
+        <section className="mt-32">
+            <div className="mb-16">
+                <h2 className="font-semibold text-4xl mb-4">Work Experience</h2>
+            </div>
+
+            <div className="flex flex-col gap-y-12">
+                {job.map((data) => (
+                    <div
+                        key={data._id}
+                        className="flex items-start lg:gap-x-6 gap-x-4 max-w-5xl relative before:absolute before:bottom-0 before:top-[4.5rem] before:left-7 before:w-[1px] before:h-[calc(100%-50px)] before:bg-zinc-800"
+                    >
+                        <a
+                            href={data.logo}
+                            rel="noreferrer noopener"
+                            className="min-h-[60px] min-w-[60px] rounded-md overflow-clip relative"
+                        >
+                            <Image
+                                src={data.logo}
+                                className="object-cover"
+                                alt={`${data.name} logo`}
+                                fill
+                            />
+                        </a>
+                        <div className="flex flex-col items-start space-y-2">
+                            <h3 className="text-xl font-bold">{data.name}</h3>
+                            <p>{data.jobTitle}</p>
+                            <small className="text-sm text-zinc-500 mt-2 tracking-widest uppercase">
+                                {data.startDate.toString()} to{" "}
+                                {data.endDate.toString()}
+                            </small>
+                            <PortableText value={data.description}/>
+                            {/* <ul className="space-y-1">
+                                {data.description
+                                    .toString()
+                                    .split("\n")
+                                    .map((point, index) => (
+                                        <li key={index}>{point}</li>
+                                    ))}
+                            </ul> */}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+}
---
+++ components/Projects.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+import { getProject } from "@/sanity/sanity.query";
+import type { ProjectType } from "@/types";
+import { PortableText } from "@portabletext/react";
+
+export default async function Project() {
+    const projects: ProjectType[] = await getProject();
+    return (
+        <section className="mt-32">
+            <div className="mb-16">
+                <h2 className="font-semibold text-4xl mb-4">Projects</h2>
+            </div>
+            <section className="grid grid-cols-1 gap-5 mb-12">
+                {projects.map((project) => (
+                    <div key={project._id}>
+                        <Image
+                            src={project.coverImage.image}
+                            width={60}
+                            height={60}
+                            alt={project.coverImage.alt}
+                            className="bg-zinc-800 rounded-md p-2"
+                        />
+                        <div>
+                            <h2 className="font-semibold mb-1">
+                                {project.name}
+                            </h2>
+                        </div>
+                        <PortableText value={project.description} />
+                    </div>
+                    //   </Link>
+                ))}
+            </section>
+        </section>
+    );
+}
