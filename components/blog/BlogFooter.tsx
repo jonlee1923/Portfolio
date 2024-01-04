06e513c43d53f0ca7370a2bfa2e4d6c2c73d9e97
Jonathan <jonlee121198@gmail.com>
2024-01-05 00:13:07 +0800

    deployment ready

---
+++ components/blog/BlogFooter.tsx
@@ -0,0 +1,12 @@
+export default function BlogFooter() {
+    return (
+        <footer className="bg-slate-900">
+            <div className="w-5/6 mx-auto flex justify-end gap-y-4 px-6 py-16 text-slate-500">
+                <small className="duration-200 text-xl">
+                    Prepared by{" "}
+                    <span className="text-indigo-400">Jonathan Lee</span>
+                </small>
+            </div>
+        </footer>
+    );
+}
