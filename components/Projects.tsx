3cf854fc7347ef502fc9f78c34e6243278d47412
Jonathan <jonlee121198@gmail.com>
2024-01-06 11:22:20 +0800

    removed empty projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,28 +8,34 @@ export default async function Project() {
     const projects: ProjectType[] = await getProject();
     return (
         <section className="mt-32">
-            <div className="flex justify-center mb-16">
-                <h2 className="font-semibold text-4xl mb-4">Projects</h2>
-            </div>
-            <section className="flex justify-center gap-5 pb-12">
-                {projects.map((project) => (
-                    <div key={project._id}>
-                        <Image
-                            src={project.coverImage.image}
-                            width={60}
-                            height={60}
-                            alt={project.coverImage.alt}
-                            className="bg-zinc-800 rounded-md p-2"
-                        />
-                        <div>
-                            <h2 className="font-semibold mb-1">
-                                {project.name}
-                            </h2>
-                        </div>
-                        <PortableText value={project.description} />
+            {projects && (
+                <div>
+                    <div className="flex justify-center mb-16">
+                        <h2 className="font-semibold text-4xl mb-4">
+                            Projects
+                        </h2>
                     </div>
-                ))}
-            </section>
+                    <section className="flex justify-center gap-5 pb-12">
+                        {projects.map((project) => (
+                            <div key={project._id}>
+                                <Image
+                                    src={project.coverImage.image}
+                                    width={60}
+                                    height={60}
+                                    alt={project.coverImage.alt}
+                                    className="bg-zinc-800 rounded-md p-2"
+                                />
+                                <div>
+                                    <h2 className="font-semibold mb-1">
+                                        {project.name}
+                                    </h2>
+                                </div>
+                                <PortableText value={project.description} />
+                            </div>
+                        ))}
+                    </section>
+                </div>
+            )}
         </section>
     );
 }
