8fb1c115ca04d8405c4611b1527205882b50ac00
Jonathan <jonlee121198@gmail.com>
2024-01-08 10:37:10 +0800

    added mobile responsiveness

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import AboutMe from "@/components/AboutMe";
+import Footer from "@/components/Footer";
 import Jobs from "@/components/Jobs";
 import Navigation from "@/components/Navigation";
 import Projects from "@/components/Projects";
@@ -13,7 +14,7 @@ export default async function Home() {
                 {profile &&
                     profile.map((data) => (
                         <div key={data._id} className="">
-                            <h1 className="flex justify-center text-6xl font-bold tracking-tight mb-6 lg:leading-[3.7rem] leading-tight min-w-full">
+                            <h1 className="flex justify-center text-4xl sm:text-6xl font-bold tracking-tight mb-6 lg:leading-[3.7rem] leading-tight min-w-full">
                                 {data.fullName}
                             </h1>
                             <h1 className="flex justify-center text-xl mb-6">
@@ -26,10 +27,11 @@ export default async function Home() {
                         </div>
                     ))}
             </section>
-            <section className="bg-gradient-to-r from-blue-300 to-pink-100">
+            <section className="bg-gradient-to-r from-blue-300 to-gray-100">
                 <AboutMe />
                 <Jobs />
                 <Projects />
+                <Footer/>
             </section>
         </main>
     );
